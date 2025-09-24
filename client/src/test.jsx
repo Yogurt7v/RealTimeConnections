3a1b8881dbdd86811e37cac94febf4e1bd5a3200
Yogurt7v <egorov-2k@yandex.ru>
2025-09-24 20:40:02 +0300

    3е задние сделано

--- client/src/test.jsx
+++ client/src/test.jsx
@@ -1,8 +1,24 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => {
+    const saved = localStorage.getItem('counter');
+    if (saved) {
+      const parsed = parseInt(saved, 10);
+      if (isNaN(parsed)) {
+        return 0;
+      } else {
+        return parsed > 10 ? 0 : parsed;
+      }
+    } else {
+      return 0;
+    }
+  });
   const ref = useRef(null);
+
+  useEffect(() => {
+    localStorage.setItem('counter', count);
+  }, [count]);
 
   return (
     <>
