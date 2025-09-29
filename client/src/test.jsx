0b691c6c6ca5864b04a5784ae45fec1e541c2ce1
Yogurt7v <egorov-2k@yandex.ru>
2025-09-29 20:57:50 +0300

    ежедневная тренировка  1) lvl done

--- client/src/test.jsx
+++ client/src/test.jsx
@@ -1,39 +1,44 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
-export default function Counter() {
-  const [count, setCount] = useState(() => {
-    const saved = localStorage.getItem('counter');
-    if (saved) {
-      const parsed = parseInt(saved, 10);
-      if (isNaN(parsed)) {
-        return 0;
-      } else {
-        return parsed > 10 ? 0 : parsed;
-      }
-    } else {
-      return 0;
+export default function ToDoList() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim()) {
+      setTodoList((prev) => [...prev, newTodo]);
+      setNewTodo('');
     }
-  });
-  const ref = useRef(null);
-
-  useEffect(() => {
-    localStorage.setItem('counter', count);
-  }, [count]);
+  };
 
   return (
     <>
-      <button onClick={() => setCount(count + 1)} disabled={count >= 10} ref={ref}>
-        Нажми на меня
-      </button>
-      <div>{count}</div>
-      <button
-        onClick={() => {
-          setCount(0);
-          ref.current.focus();
-        }}
-      >
-        Сбросить
-      </button>
+      <div>
+        <input
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              addTodo();
+            }
+          }}
+          placeholder="Добавь новое дело"
+          onChange={(e) => {
+            setNewTodo(e.target.value);
+          }}
+          value={newTodo}
+        />
+        <button onClick={() => addTodo()}>Добавить</button>
+      </div>
+      <div>
+        {todoList.length === 0 ? (
+          <div>"Список дел пуст"</div>
+        ) : (
+          <div>
+            {todoList.map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </div>
+        )}
+      </div>
     </>
   );
 }
