2cd68402f312c60e8cae1455c35bd605b5d8040e
Yogurt7v <egorov-2k@yandex.ru>
2025-10-02 20:45:22 +0300

    timer с остановкой, паузой и ресетом

--- client/src/test.jsx
+++ client/src/test.jsx
@@ -3,11 +3,34 @@ import { useEffect, useRef, useState } from 'react';
 export default function Countdown({ initialSeconds = 64 }) {
   const [timer, setTimer] = useState(+initialSeconds);
   const timerFunction = useRef(null);
+  const [stopped, setStopped] = useState(false);
 
   const formatTime = (seconds) => {
     const mins = Math.floor(seconds / 60);
     const secs = seconds % 60;
     return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+  };
+
+  const startTimer = () => {
+    if (timerFunction.current) return; // Если уже запущен
+    timerFunction.current = setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000);
+    setStopped(false);
+  };
+
+  const stopTimer = () => {
+    if (timerFunction.current) {
+      clearInterval(timerFunction.current);
+      timerFunction.current = null;
+      setStopped(true);
+    }
+  };
+
+  const reset = () => {
+    if (timer > 0) {
+      setTimer(initialSeconds);
+    }
   };
 
   useEffect(() => {
@@ -24,5 +47,15 @@ export default function Countdown({ initialSeconds = 64 }) {
     return () => clearInterval(timerFunction.current);
   }, []);
 
-  return <div>{formatTime(timer)}</div>;
+  return (
+    <>
+      <div>{formatTime(timer)}</div>
+      <div>
+        <button onClick={reset}>Сбросить</button>
+      </div>
+      <button onClick={stopped ? startTimer : stopTimer}>
+        {!stopped ? <span>Пауза</span> : <span>Возобновить</span>}
+      </button>
+    </>
+  );
 }
